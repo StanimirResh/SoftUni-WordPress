126abff28c37d35506af2fe1acefb5443b58e2b1
Stanimir Reshavski <mchorko@gmail.com>
2024-01-16 16:27:38 +0200

    Added 404.php and registering menus

---
+++ wp-content/themes/softuni/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+
+<!-- Product -->
+<div class="intro row">
+	<div class="col-sm-6 col-sm-offset-6">
+		<h2 class="header-quote">404 - Sorry, not found!</h2>
+	</div>
+</div> <!-- /.container -->
+</section> <!-- /#header -->
+
+
+<?php get_footer() ?>
--- wp-content/themes/softuni/functions.php
+++ wp-content/themes/softuni/functions.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * Never worry about cache again!
  */
-function load_assets( $hook ) {
+function load_assets($hook) {
 	// create my own version codes
 	// $my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'js/custom.js' ));
 	// $my_css_ver = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'style.css' ));
@@ -12,16 +13,29 @@ function load_assets( $hook ) {
 		'strategy' => 'defer',
 	);
 
-	wp_enqueue_script( 'bootstrap-min-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '1.0.0', $args );
-	wp_enqueue_script( 'jquery.magnific-popup', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js', array(), '1.0.0', $args );
-	wp_enqueue_script( 'owl.carousel', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array(), '1.0.0', $args );
+	wp_enqueue_script('bootstrap-min-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '1.0.0', $args);
+	wp_enqueue_script('jquery.magnific-popup', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js', array(), '1.0.0', $args);
+	wp_enqueue_script('owl.carousel', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array(), '1.0.0', $args);
 
-	wp_enqueue_style( 'bootstrap.min-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', false, '1.0.0' );
-	wp_enqueue_style( 'ionicons.css', get_template_directory_uri() . '/assets/css/ionicons.css', false, '1.0.0' );
-	wp_enqueue_style( 'magnific-popup.css', get_template_directory_uri() . '/assets/css/magnific-popup.css', false, '1.0.0' );
-	wp_enqueue_style( 'main.css', get_template_directory_uri() . '/assets/css/main.css', false, '1.0.2' );
-	wp_enqueue_style( 'owl.carousel.css', get_template_directory_uri() . '/assets/css/owl.carousel.css', false, '1.0.0' );
-	wp_enqueue_style( 'owl.carousel.theme.min.css', get_template_directory_uri() . '/assets/css/owl.carousel.theme.min.css', false, '1.0.0' );
-
+	wp_enqueue_style('bootstrap.min-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', false, '1.0.0');
+	wp_enqueue_style('ionicons.css', get_template_directory_uri() . '/assets/css/ionicons.css', false, '1.0.0');
+	wp_enqueue_style('magnific-popup.css', get_template_directory_uri() . '/assets/css/magnific-popup.css', false, '1.0.0');
+	wp_enqueue_style('main.css', get_template_directory_uri() . '/assets/css/main.css', false, '1.0.2');
+	wp_enqueue_style('owl.carousel.css', get_template_directory_uri() . '/assets/css/owl.carousel.css', false, '1.0.0');
+	wp_enqueue_style('owl.carousel.theme.min.css', get_template_directory_uri() . '/assets/css/owl.carousel.theme.min.css', false, '1.0.0');
 }
-add_action( 'wp_enqueue_scripts', 'load_assets' );
+add_action('wp_enqueue_scripts', 'load_assets');
+
+
+function softuni_register_nav_menus() {
+	register_nav_menus(
+		array(
+			'primary_menu' => __('Primary Menu', 'softuni'),
+			'popular_services' => __('Popular Services', 'softuni'),
+			'important_link' => __('Important Links', 'softuni'),
+			'latest_services' => __('Latest Services', 'softuni')
+		)
+	);
+}
+
+add_action('after_setup_theme ', 'softuni_register_nav_menus');
