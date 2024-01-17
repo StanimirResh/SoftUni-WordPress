2a2303f370f9d382b68abe8311ab4882cf11fd14
Stanimir Reshavski <mchorko@gmail.com>
2024-01-17 14:09:39 +0200

    author.php added (identical to archive.php

---
+++ wp-content/themes/softuni/archive.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<div class="intro row">
+	<div class="overlay"></div>
+	<div class="col-sm-12">
+		<ol class="breadcrumb">
+			<li><a href="<?php get_home_url() ?>">Home</a></li>
+			<li class="active"><?php the_archive_title() ?></li>
+		</ol>
+	</div>
+</div> <!-- /.intro.row -->
+</div>
+<div class="nutral"></div>
+</section>
+
+<section class="faq">
+	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) :
+			the_post(); ?>
+
+			<?php
+			get_template_part('partials/content', 'post')
+			?>
+
+		<?php endwhile; ?>
+	<?php else : ?>
+		Sorry, there is no posts here.
+	<?php endif; ?>
+</section>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/softuni/partials/content-post.php
@@ -0,0 +1,15 @@
+<div class="container page-bgc">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="title-box">
+                <h2 class="title mt0">
+                    <?php the_title(); ?>
+                </h2>
+                <p class="inner-p">
+                    <?php the_content(); ?>
+                </p>
+            </div>
+        </div>
+
+    </div>
+</div>
